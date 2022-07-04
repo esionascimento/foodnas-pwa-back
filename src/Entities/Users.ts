6f4ffd5f9242cdef40fe162fb4fa9dd0ef708fbf
esionascimento <esionascimento@protonmail.com>
2022-07-04 13:52:39 -0400

    create entity users

---
+++ src/Entities/Users.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, BaseEntity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Users extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+}
