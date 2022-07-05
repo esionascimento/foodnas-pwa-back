37b4692637ae7eafaf941da568f75bffb2111fc7
esionascimento <esionascimento@protonmail.com>
2022-07-05 15:44:53 -0400

    create: Mutation DELETE_USER_ID

--- src/schema/Mutations/User.ts
+++ src/schema/Mutations/User.ts
@@ -1,4 +1,4 @@
-import { GraphQLString } from 'graphql';
+import { GraphQLBoolean, GraphQLID, GraphQLString } from 'graphql';
 import { Users } from '../../Entities/Users';
 import { UserType } from '../typeDefs/User';
 import bcrypt from 'bcryptjs';
@@ -24,3 +24,16 @@ export const CREATE_USER = {
     return {...args, id: result.identifiers[0].id, password: encrypt}
   }
 }
+
+export const DELETE_USER_ID = {
+  type: GraphQLBoolean,
+  args: {
+    id: { type: GraphQLID }
+  },
+  async resolve(_: any, {id}: any) {
+    const result =  await Users.delete(id);
+
+    if (result.affected === 1) return true
+    return false
+  }
+};
