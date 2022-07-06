5bfdaaa1da517137ca6e1139d59211a7967e506c
esionascimento <esionascimento@protonmail.com>
2022-07-05 22:10:09 -0400

    config: news Mutations

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,6 +1,6 @@
 import { GraphQLSchema, GraphQLObjectType } from "graphql";
 import { GREETING } from './Queries/Greeting';
-import { CREATE_USER, DELETE_USER_ID } from './Mutations/User';
+import { CREATE_USER, DELETE_USER_ID, UPDATE_USER } from './Mutations/User';
 import { GET_ALL_USER, GET_USER_ID } from './Queries/User';
 
 const RootQuery = new GraphQLObjectType({
@@ -16,7 +16,8 @@ const Mutation = new GraphQLObjectType({
   name: "Mutation",
   fields: {
     createUser: CREATE_USER,
-    deleteUserId: DELETE_USER_ID
+    deleteUserId: DELETE_USER_ID,
+    updateUser: UPDATE_USER
   }
 })
 
