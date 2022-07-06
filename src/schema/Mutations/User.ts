77e29c59f9be067e0a0540e61c3f42227e6440fb
esionascimento <esionascimento@protonmail.com>
2022-07-05 22:09:14 -0400

    create: new Mutation UPDATE_USER

--- src/schema/Mutations/User.ts
+++ src/schema/Mutations/User.ts
@@ -1,6 +1,7 @@
-import { GraphQLBoolean, GraphQLID, GraphQLString } from 'graphql';
+import { GraphQLBoolean, GraphQLID, GraphQLInputObjectType, GraphQLString } from 'graphql';
 import { Users } from '../../Entities/Users';
 import { UserType } from '../typeDefs/User';
+import { MessageType } from '../typeDefs/Message';
 import bcrypt from 'bcryptjs';
 
 export const CREATE_USER = {
@@ -35,5 +36,52 @@ export const DELETE_USER_ID = {
 
     if (result.affected === 1) return true
     return false
+  }
+};
+
+export const UPDATE_USER = {
+  type: MessageType,
+  args: {
+    id: { type: GraphQLID },
+    input: {
+      type: new GraphQLInputObjectType({
+        name: "UserInput",
+        fields: {
+          name: { type: GraphQLString },
+          username: { type: GraphQLString },
+          oldPassword: { type: GraphQLString },
+          newPassword: { type: GraphQLString }
+        }
+      })
+    }
+  },
+  async resolve(_: any, {id, input}: any) {
+    const resultExist = await Users.findOneBy({id});
+    if (!resultExist) return {
+      success: false,
+      message: 'User not found'
+    }
+
+    const isMatch = await bcrypt.compare(input.oldPassword, resultExist.password);
+    if (!isMatch) return {
+      success: false,
+      message: 'Failed to compare old password'
+    }
+
+    const newPasswordHash = await bcrypt.hash(input.newPassword, 12);
+    const result = await Users.update({id}, {
+      name: input.name,
+      username: input.username,
+      password: newPasswordHash
+    });
+    if (result.affected === 0) return {
+      success: false,
+      message: 'User not successfully updated'
+    }
+
+    return {
+      success: true,
+      message: 'Successfully updated'
+    }
   }
 };
